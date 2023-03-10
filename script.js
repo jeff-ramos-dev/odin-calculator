2eabf925f7c13e719a6bfb638efafe390f950f1c
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-03-09 22:15:55 -0800

    Add js variables

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const allClearBtn = document.querySelector('.ac')
+const clearBtn = document.querySelector('.c')
+const posNegBtn = document.querySelector('.pos-neg')
+const divideBtn = document.querySelector('.divide')
+const multiplyBtn = document.querySelector('.multiply')
+const minusBtn = document.querySelector('.subtract')
+const plusBtn = document.querySelector('.add')
+const equalBtn = document.querySelector('.equal')
+const decimalBtn = document.querySelector('.decimal')
+const zeroBtn = document.querySelector('.zero')
+const oneBtn = document.querySelector('.one')
+const twoBtn = document.querySelector('.two')
+const threeBtn = document.querySelector('.three')
+const fourBtn = document.querySelector('.four')
+const fiveBtn = document.querySelector('.five')
+const sixBtn = document.querySelector('.six')
+const sevenBtn = document.querySelector('.seven')
+const eightBtn = document.querySelector('.eight')
+const nineBtn = document.querySelector('.nine')
+
+
 function add(x, y) {
     return x + y
 }
