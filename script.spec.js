e60767e2713f8b29e891fd61803aeeeef5933522
Joel <7258285+ramosjoel@users.noreply.github.com>
2023-03-19 20:10:41 -0700

    Fixes jest tests

--- script.spec.js
+++ script.spec.js
@@ -1,55 +1,66 @@
-const calculator = require('./script')
+/**
+ * @jest-environment jsdom
+ */
+const fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const htmlStr = fs.readFileSync('index.html', 'utf8');
+const script = fs.readFileSync('script.js', 'utf8');
+const { window } = new JSDOM(
+    `<body><script>${script}</script></body>`,
+    { runScripts: "dangerously", resources: "usable" }
+);
 
 describe('add', () => {
 	test('adds 0 and 0', () => {
-		expect(calculator.add(0,0)).toBe(0);
+		expect(window.add(0,0)).toBe(0);
 	});
 
 	test('adds 2 and 2', () => {
-		expect(calculator.add(2,2)).toBe(4);
+		expect(window.add(2,2)).toBe(4);
 	});
 
 	test('adds positive numbers', () => {
-		expect(calculator.add(2,6)).toBe(8);
+		expect(window.add(2,6)).toBe(8);
 	});
 });
 
 describe('subtract', () => {
 	test('subtracts numbers', () => {
-		expect(calculator.subtract(10,4)).toBe(6);
+		expect(window.subtract(10,4)).toBe(6);
 	});
 });
 
 
 describe('multiply', () => {
 	test('multiplies two numbers', () => {
-		expect(calculator.multiply(2,4)).toBe(8);
+		expect(window.multiply(2,4)).toBe(8);
 	});
 
 	test('multiplies negative numbers', () => {
-		expect(calculator.multiply(-2,4)).toBe(-8);
+		expect(window.multiply(-2,4)).toBe(-8);
 	});
 });
 
 describe('operate', () => {
     test('performs addition', () => {
-        expect(calculator.operate("add", 2, 4)).toBe(6)
+        expect(window.operate("add", 2, 4)).toBe(6)
     })
 
     test('performs subtraction', () => {
-        expect(calculator.operate("subtract", 12, 3)).toBe(9)
+        expect(window.operate("subtract", 12, 3)).toBe(9)
     })
 
     test('performs multiplication', () => {
-        expect(calculator.operate("multiply", 12, 4)).toBe(48)
+        expect(window.operate("multiply", 12, 4)).toBe(48)
     })
 
     test('performs division', () => {
-        expect(calculator.operate("divide", 12, 4)).toBe(3)
+        expect(window.operate("divide", 12, 4)).toBe(3)
     })
 
     test('returns error message with invalid operator', () => {
-        expect(calculator.operate("power", 2, 3)).toBe("Invalid input")
+        expect(window.operate("power", 2, 3)).toBe("Invalid input")
     })
 })
 
